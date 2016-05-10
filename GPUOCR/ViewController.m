233d88fdd166aaa718cbcad54e93ac0a62801195
Chris Hanshew <chrishanshew@gmail.com>
2016-05-10 14:52:20 -0600

    Initial Commit

---
+++ GPUOCR/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GPUOCR
+//
+//  Created by Chris Hanshew on 5/10/16.
+//  Copyright © 2016 Chris Hanshew. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ GPUOCRTests/GPUOCRTests.m
@@ -0,0 +1,39 @@
+//
+//  GPUOCRTests.m
+//  GPUOCRTests
+//
+//  Created by Chris Hanshew on 5/10/16.
+//  Copyright © 2016 Chris Hanshew. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GPUOCRTests : XCTestCase
+
+@end
+
+@implementation GPUOCRTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
